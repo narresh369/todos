7a46fe266704072e7bdb75326bc2486ca9d3730c
narresh369 <venkatdada369@gmail.com>
2025-06-29 14:25:56 +0530

    Updated files Edited Dockerfile with ignore file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 
 const API_BASE = import.meta.env.VITE_API_URL;
 
+console.log('API_BASE : ' + API_BASE);
+
 function App() {
   const [todos, setTodos] = useState([]);
   const [title, setTitle] = useState('');
